8f307891aa58503d4d34fe0cefa2beb2d4188346
Alirh1 <alirhayem.1212@gmail.com>
2024-07-18 00:52:28 +0300

    solved error

--- connection.php
+++ connection.php
@@ -3,9 +3,8 @@ $servername = "localhost";
 $username = "root";
 $password = '';
 $db_name = 'flightmanagementsystem';
-$port = 4306;
 
-$conn = new mysqli($servername, $username, $password, $db_name,$port );
+$conn = new mysqli($servername, $username, $password, $db_name );
 
 if ($conn->connect_error) {
     die('connection failed' . $conn->connect_error);
